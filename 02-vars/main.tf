784f4ba60a2c475eb1e0ae9efa6d6d45af307986
gv <gvdevops00@gmail.com>
2022-07-22 19:10:46 +0530

    auto pushing chainges repo

--- 02-vars/main.tf
+++ 02-vars/main.tf
@@ -11,7 +11,19 @@ output "a" {
 // variable in terraform can be accessed by var.var_name or ${var.var_name}
 
 output "x" {
-  value = "Value of a =${var.a}"
+  value = "Value of a = ${var.a}"
+
+output "Values" {
+  value = "Values, a = ${var.a} b = ${var.b} c = ${var.c} d = ${var.d}"
 }
 
 //If you access variable with any combination of other strings then ${} is preferred
+
+variable "b" {}
+// this variable b is declared in tfvars file
+
+variable "c" {}
+// this variable from cli of terraform , terraform apply -auto-approve -var c=400
+
+variable "d" {}
+// this variable from SHELL, export TF_VAR_d=500
