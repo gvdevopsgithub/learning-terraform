6aaa08cbd529762b1977b792bd9e3038dab7c03c
gv <gvdevops00@gmail.com>
2022-07-24 12:12:17 +0530

    auto pushing chainges repo

--- 04-loops/approach01/main.tf
+++ 04-loops/approach01/main.tf
@@ -1,12 +1,9 @@
-variable "no-of-instances" {
+resource "aws_instance" "web" {
   default = 2
 }
-
-resource "aws_instance" "web" {
-  count         = var.no-of-instances
+  resource "aws_instance" "web" {
   ami           = "ami-0f234acd4850f57e2"
   instance_type = "t3.micro"
-
 
   tags = {
     Name = "terraform"
