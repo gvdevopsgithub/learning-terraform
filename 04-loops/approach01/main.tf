e200b8393dea8850c04a6c304ce6b54a86946af1
gv <gvdevops00@gmail.com>
2022-07-24 12:43:59 +0530

    auto pushing chainges repo

--- 04-loops/approach01/main.tf
+++ 04-loops/approach01/main.tf
@@ -2,7 +2,7 @@ variable "aws_instance" {
   default = 2
 }
 resource "aws_instance" "web" {
-  count         = var.no-of-instances
+  count         = var."no-of-instances" {}
   ami           = "ami-0f234acd4850f57e2"
   instance_type = "t3.micro"
 
