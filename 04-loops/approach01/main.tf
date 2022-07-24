05438a09632a7e921083a48e313c3bfb08f50bf4
gv <gvdevops00@gmail.com>
2022-07-24 18:02:52 +0530

    auto pushing chainges repo

--- 04-loops/approach01/main.tf
+++ 04-loops/approach01/main.tf
@@ -4,12 +4,12 @@ resource "aws_instance" "web" {
   instance_type = var.instance_types[count.index]
 
   tags = {
-    Name = "terraform-${count.index + 1}"
+    Name = "terraform-${count.index+1}"
   }
 }
 
 variable "instance_types" {
-  default = ["t3.micro" , "t3.small"]
+  default = ["t3.micro", "t3.small"]
 }
 
 // requirement is , Create number of instances og the given instance types
