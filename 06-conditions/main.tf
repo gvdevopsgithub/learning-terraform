c99175bba9763f18b5844330362c0ce476f166a0
gv <gvdevops00@gmail.com>
2022-07-26 07:17:55 +0530

    auto pushing chainges repo

--- 06-conditions/main.tf
+++ 06-conditions/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   // count      = 0 (we can make some condition to come with a value based on that this resource can be created)
-  ami           = "ami-0f234acd4850f57e2"
-  instance_type = var.instance_type == null ? "t3.micro" : var.instance_type
+  ami           = data.aws_ami.example.
+  instance_type =  local.instance_type
 
   tags = {
     Name = "terraform"
@@ -10,4 +10,15 @@ resource "aws_instance" "web" {
 
 variable "instance_type" {
   default = null
+}
+
+locals {
+  instance_type = var.instance_type == null ? "t3.micro" : var.instance_type
+}
+
+data "aws_ami" "example" {
+executable_users = ["self"]
+most_recent      = true
+name_regex       = "base-with-ansible"
+owners           = ["self"]
 }
