cab8a75533ff8b7bb82e6d5f83502f543c887bfd
gv <gvdevops00@gmail.com>
2022-10-11 15:02:45 +0530

    push

--- 02-vars/main.tf
+++ 02-vars/main.tf
@@ -13,38 +13,3 @@ output "a" {
 output "x" {
   value = "Value of a = ${var.a}"
 }
-
-output "Values" {
-  value = "Values, a = ${var.a} b = ${var.b} c = ${var.c} d = ${var.d}"
-}
-
-//If you access variable with any combination of other strings then ${} is preferred
-
-variable "b" {}
-// this variable b is declared in tfvars file
-
-variable "c" {}
-// this variable from cli of terraform , terraform apply -auto-approve -var c=400
-
-variable "d" {}
-// this variable from SHELL, export TF_VAR_d=500
-
-variable "sample" {
-  default = [100, 200, "abc"]
-}
-
-output "sample" {
-  value = "var.sample[1]"
-}
-
-
-variable "sample1" {
-  default = {
-    abc = 100
-    xyz = 200
-  }
-}
-
-output "sample1" {
-  value = "var.sample1[xyz]"
-}
