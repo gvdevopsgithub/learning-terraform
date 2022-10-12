2160960269d9bf86e6fd59cc604c1eaa5c9bffe6
gv <gvdevops00@gmail.com>
2022-10-12 21:29:33 +0530

    push

--- 01-outputs/main.tf
+++ 01-outputs/main.tf
@@ -1,9 +1,6 @@
-#output "sample" {
-#	value = "Hello World"
-#}
-
-// output block requires one reference, here sample is reference
-
 output "sample" {
   value = "Hello World"
 }
+
+// output block requires one reference, here sample is reference
+
