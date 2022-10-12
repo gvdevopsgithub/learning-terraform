94c2e00ca5dd42c0f01a26c8d975a6128a6a60ad
gv <gvdevops00@gmail.com>
2022-10-12 16:18:38 +0530

    push

--- 02-vars/main.tf
+++ 02-vars/main.tf
@@ -37,16 +37,16 @@ output "sample" {
   value = "var.sample[1]"
 }
 
-
-variable "sample1" {
-  default = {
-    abc = 100
-    xyz = 200
-  }
-}
-
-output "sample1" {
-  value = "var.sample1[xyz]"
-}
+#
+#variable "sample1" {
+#  default = {
+#    abc = 100
+#    xyz = 200
+#  }
+#}
+#
+#output "sample1" {
+#  value = "var.sample1[xyz]"
+#}
 
 
