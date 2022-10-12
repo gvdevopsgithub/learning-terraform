d66bfa62ce8b8201a430707680be50ad1b538c41
gv <gvdevops00@gmail.com>
2022-10-12 23:43:32 +0530

    push

--- 03-resources/main.tf
+++ 03-resources/main.tf
@@ -1,3 +1,8 @@
+#resource "local_file" "foo" {
+#    content  = "foo!"
+#    filename = "${path.module}/foo.bar"
+#}
+
 resource "local_file" "foo" {
     content  = "foo!"
     filename = "${path.module}/foo.bar"
