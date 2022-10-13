e63eeff8a3b3e7ce8eedd3d7659d0185af3e146c
gv <gvdevops00@gmail.com>
2022-10-13 12:08:33 +0530

    push

--- 04-loops/approach01/main.tf
+++ 04-loops/approach01/main.tf
@@ -1,16 +1,31 @@
+#variable "instance_types" {
+#  default = 2
+#}
+#
+#resource "aws_instance" "web" {
+#  count         = "2"
+#  ami           = "ami-00d7fca492f096d8c"
+#  instance_type = "t3.micro"
+#
+#  tags = {
+#    Name = "terraform-${count.index+1}"
+#  }
+#}
+#output "public_ip_address" {
+#  value = aws_instance.web.*.public_ip
+#}
+
+
 variable "instance_types" {
-  default = 2
+  default = 3
 }
 
 resource "aws_instance" "web" {
-  count         = "2"
+  count         = "3"
   ami           = "ami-00d7fca492f096d8c"
   instance_type = "t3.micro"
 
   tags = {
-    Name = "terraform-${count.index+1}"
+    Name = "terraform"
   }
-}
-output "public_ip_address" {
-  value = aws_instance.web.*.public_ip
 }
