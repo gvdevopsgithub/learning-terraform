aac3c87c71c41412db04a4f4c1832e3299b07049
gv <gvdevops00@gmail.com>
2022-10-14 15:50:05 +0530

    push

--- 05-functions/main.tf
+++ 05-functions/main.tf
@@ -1,18 +1,27 @@
+#variable "alist" {
+#  default = [1, 2]
+#}
+#
+#variable "amap" {
+#  default = {
+#    a = 1
+#    b = 2
+#  }
+#}
+#
+#output "alist_length" {
+#  value = length(var.alist)
+#}
+#
+#output "amap_length" {
+#  value = length(var.amap)
+#}
+
+
 variable "alist" {
   default = [1, 2]
 }
 
-variable "amap" {
-  default = {
-    a = 1
-    b = 2
-  }
-}
-
 output "alist_length" {
   value = length(var.alist)
-}
-
-output "amap_length" {
-  value = length(var.amap)
 }
